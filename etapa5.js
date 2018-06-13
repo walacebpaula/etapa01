4d0c5739cffa9e55f01e21b2bd6fc7d97d86083f
walacebpaula <39489190+walacebpaula@users.noreply.github.com>
2018-06-13 10:27:01 -0300

    Create etapa5.js
    etapa criada em atraso neste repositório pelo mesmo motivo da etapa 3

---
+++ etapa5.js
@@ -0,0 +1,17 @@
+var vidas = 10; 
+var pontos = 0; 
+var nivel = 3
+
+function setup() {
+  createCanvas(490,510);
+
+}
+
+function draw() {
+  background(200); 
+  textSize(15);
+  fill(5,0,0);
+  text("Vidas: "+vidas, 5, 60);
+  text("Pontos: "+pontos, 5, 80);
+  text("Nivel: "+nivel, 5, 100);
+}
